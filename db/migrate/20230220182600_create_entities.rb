62eb388004f7f377c8b032de69aa01a06b1b99f6
vitalis <vitalismutwiri2gmail.com>
2023-02-20 21:50:17 +0300

    Added User, Groups, Entity and joining table

---
+++ db/migrate/20230220182600_create_entities.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.decimal :amount
+
+      t.timestamps
+    end
+  end
+end
