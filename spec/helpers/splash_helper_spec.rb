d0afdc8adce5dfd7f56a1b6ad2012ab68c5c7eb7
vitalis <vitalismutwiri2gmail.com>
2023-02-22 22:45:35 +0300

    Linter Correction

--- spec/helpers/splash_helper_spec.rb
+++ spec/helpers/splash_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper' # rubocop :disable  Layout/EndOfLine
+require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
 # the SplashHelper. For example:
